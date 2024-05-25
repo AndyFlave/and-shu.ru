12afa76bc5fc37cd5527dda7dd239aa58d2cddb1
Андрей Шушунов <a.shushunov@runetsoft.ru>
2024-05-25 09:31:55 +0300

    chore: [#1] added yam

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,6 +16,31 @@ export default {
     link: [
       { rel: 'icon', type: 'image/x-icon', href: 'src/static/favicon.ico' },
     ],
+
+    script: [
+      {
+        innerHTML: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+          m[i].l=1*new Date();
+          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+      
+          ym(92329310, "init", {
+              clickmap:true,
+              trackLinks:true,
+              accurateTrackBounce:true,
+              webvisor:true
+          })`,
+      },
+    ],
+
+    noscript: [
+      {
+        innerHTML:
+          '<div><img src="https://mc.yandex.ru/watch/92329310" style="position:absolute; left:-9999px;" alt="" /></div>',
+      },
+    ],
+    __dangerouslyDisableSanitizers: ['script', 'noscript'],
   },
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
